5942f640bc5057e37a54d10ba6b66dd65a2c4ee8
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-07-30 00:47:54 +0300

    changed component IconArrow
    replace svg with padding
    update styled params

--- src/components/shared/Icons/IconArrow.js
+++ src/components/shared/Icons/IconArrow.js
@@ -3,24 +3,37 @@ import { ReactComponent as KeyboardArrowSvg } from "./../../../assets/icons/keyb
 import { defaultTheme } from "../../../theme/default";
 
 export const IconKeyboardArrowDown = styled(KeyboardArrowSvg)`
-
-    cursor:pointer;
-
-    :hover {
-        fill:${defaultTheme.colorButtonHover};
+    width:24px;
+    height:24px;
+    path {
+        fill:${defaultTheme.colorIconDefault};
     }
-    
-    :active {
-        fill:${defaultTheme.colorButtonActive};
+    &:hover {
+        cursor: pointer;
+    }
+    &:hover path{ 
+        fill:${defaultTheme.colorIconHover};
+    }
+    &:active path{ 
+        fill:${defaultTheme.colorIconActive};
     }
 `;
 
 export const IconKeyboardArrowUp = styled(KeyboardArrowSvg)`
-    cursor:pointer;
     transform: rotate(-180deg);
-    & > path:hover {
-
-        fill:currentColor;
+    width:24px;
+    height:24px;
+    path {
+        fill:${defaultTheme.colorIconDefault};
+    }
+    &:hover {
+        cursor: pointer;
+    }
+    &:hover path{ 
+        fill:${defaultTheme.colorIconHover};
+    }
+    &:active path{ 
+        fill:${defaultTheme.colorIconActive};
     }
 `;
 
