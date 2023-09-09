07791cca40000612830dd1229685e3f6a2d63ff4
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-09 23:03:07 +0300

    update Checbox component

--- src/components/shared/ui/CheckBox/CheckBox.jsx
+++ src/components/shared/ui/CheckBox/CheckBox.jsx
@@ -1,33 +1,21 @@
-import { useState } from "react";
-import { StyledCheckBox } from "./styles";
-import { IconCheckbox, IconCheckboxChecked } from "../../Icons";
+import {useState} from 'react';
+import {StyledCheckBox} from './styles';
+import {IconCheckbox, IconCheckboxChecked} from '../../Icons';
 
-
-function CheckBox({
-  id,
-  onClick,
-  checked
-}) {
-  
+function CheckBox({id, onClick, checked}) {
   const [checkBox, setChecked] = useState(<IconCheckbox />);
 
-   return (
-    <StyledCheckBox >
-      
+  return (
+    <StyledCheckBox
+      onClick={(e) => {
+        onClick(e.target);
+      }}
+    >
       <label>
-        <input 
-          type="checkbox"
-          name="checkbox"
-          id={id}
-          onClick={ (e)=> {onClick(e.target)}}
-        />
+        <input type="checkbox" name="checkbox" id={id} />
       </label>
-      
-      {
-        checked ?
-        <IconCheckboxChecked /> : <IconCheckbox />
-       }
-     </StyledCheckBox>
+      {checked ? <IconCheckboxChecked /> : <IconCheckbox />}
+    </StyledCheckBox>
   );
 }
 
--- src/components/shared/ui/CheckBox/styles.js
+++ src/components/shared/ui/CheckBox/styles.js
@@ -1,32 +1,23 @@
-import styled from "styled-components";
-import { defaultTheme } from "../../../../theme/default";
-import { IconCheckbox } from "../../Icons";
+import styled from 'styled-components';
+import {defaultTheme} from '../../../../theme/default';
+import {IconCheckbox} from '../../Icons';
 
 export const StyledCheckBox = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position:relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
 
-input {
-    cursor:pointer;
-    width:16px;
-    height:16px;
-    opacity:0;
-    top:1px;
-    left:2px;
-    position:absolute;
-    
-}
-& input:hover {
-    opacity:0;
-    border:1px solid red;
-}
-input:hover {
-    & + ${IconCheckbox}{
-        opacity:1;
-        border:1px solid red;
-    }
-}
-
+  input {
+    cursor: pointer;
+    width: 16px;
+    height: 16px;
+    opacity: 0;
+    top: 1px;
+    left: 8px;
+    position: absolute;
+  }
+  & input:hover {
+    opacity: 0;
+  }
 `;
