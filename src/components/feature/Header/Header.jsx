4b37497bc820013bc6e59ecd291227186384721d
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:22:18 +0300

    update Header

--- src/components/feature/Header/Header.jsx
+++ src/components/feature/Header/Header.jsx
@@ -1,31 +1,73 @@
-import { 
-  AnalyticsCalls, 
-  Partner, 
-  StyledHeader, 
-  StyledHeaderWrapper, 
-  StyledIconKeyboardArrowDown, 
-  StyledIconSearch, 
-  Today 
-} from "./styles";
+import {allOrganizations} from '../../../mocks/selects';
+import Profile from '../Profile/Profile';
+import Select from '../Select/Select';
+import {
+  AnalyticsCalls,
+  Line,
+  NewCalls,
+  Text,
+  QuantityCalls,
+  СonversionToOrder,
+  StyledHeader,
+  StyledHeaderWrapper,
+  StyledIconSearch,
+  Today,
+  RightBlock,
+  LeftBlock,
+  StyledSelect,  
+} from './styles';
 
-function Header() {
+function Header({
+  newCalls = 20,
+  allCalls = 30,
+  quantityCalls = 40,
+  conversion = 67,
+}) {
   return (
-      <StyledHeader>
-        <StyledHeaderWrapper>
-          <Today>
-            Среда, 13 окт
-          </Today>
+    <StyledHeader>
+      <StyledHeaderWrapper>
+        <LeftBlock>
+          <Today>Среда, 13 окт</Today>
           <AnalyticsCalls>
+            <NewCalls>
+              <Text>
+                Новые звонки
+                <span>
+                  {' '}
+                  {newCalls} из {allCalls} шт
+                </span>
+              </Text>
+              <Line completion={newCalls / allCalls} />
+            </NewCalls>
+            <QuantityCalls>
+              <Text>
+                Качество разговоров
+                <span> {quantityCalls} % </span>
+              </Text>
+              <Line completion={quantityCalls / 100} />
+            </QuantityCalls>
+            <СonversionToOrder>
+              <Text>
+                Конверсия в заказ
+                <span> {conversion} % </span>
+              </Text>
+              <Line completion={conversion / 100} />
+            </СonversionToOrder>
           </AnalyticsCalls>
-          <StyledIconSearch />
-          <Partner >
-            ИП Сидорова Александра Михайловна
-            <StyledIconKeyboardArrowDown />
-          </Partner>
-          
-        </StyledHeaderWrapper>
+        </LeftBlock>
 
-      </StyledHeader>
+        <RightBlock>
+          <StyledIconSearch />
+          <StyledSelect>
+          <Select
+            label={allOrganizations.label}
+            options={allOrganizations.options}
+          />
+          </StyledSelect>
+          <Profile />
+        </RightBlock>
+      </StyledHeaderWrapper>
+    </StyledHeader>
   );
 }
 
--- src/components/feature/Header/styles.js
+++ src/components/feature/Header/styles.js
@@ -1,72 +1,173 @@
-import styled from "styled-components";
-import { Section } from './../../../styled/index';
-import { 
-    IconSearch,
-    IconKeyboardArrowDown 
-} from "../../shared/Icons/index";
+import styled from 'styled-components';
+import {Section} from './../../../styled/index';
+import {IconSearch, IconKeyboardArrowDown} from '../../shared/Icons/index';
+import {defaultTheme} from '../../../theme/default';
+import Select from '../Select/Select';
+
 
 export const StyledHeader = styled.header`
-    height:64px;
-    background: #FFFFFF;
-    box-shadow: 0px 4px 5px #E9EDF3;
-    width: ${(props) => props.theme.pageWidth};
-    margin: 0 auto;
+  height: 64px;
+  background: #ffffff;
+  box-shadow: 0px 4px 5px 0px #e9edf3;
+  width: ${(props) => props.theme.pageWidth};
+  margin: 0 auto;
 `;
 
 export const StyledHeaderWrapper = styled(Section)`
-    margin: 0 auto;
-    width:1440px;
-    height:100%;
-    border:1px solid red;
-    display: flex;
-    align-items: center;
+  margin: 0 auto;
+  width: 1440px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const Today = styled.p`
-    padding:0;
-    margin:0;
-    width:136px;
-    margin-right: 40px;
-    border:1px solid red;
-`;
-
-export const AnalyticsCalls = styled.div`
-    width:594px;
-    height:100%;
-    margin-right: 189px;
-    border:1px solid red;
+  padding: 0;
+  margin: 0;
+  width: 136px;
+  margin-right: 40px;
 `;
 
 export const StyledIconSearch = styled(IconSearch)`
-    padding:16px 8px;
-    border:1px solid red;
-    margin-right: 56px;
-    &:hover {
-        color:#002CFB;
-    }
-`;
+  padding: 16px 8px;
+  margin-right: 56px;
+  &:hover {
+    color: #002cfb;
+  }
 
-export const Partner = styled.div`
-    display: flex;
-    width: 300px;
-    justify-content: space-between;
-    font-weight: 400;
-    line-height:21px;
-    font-size:15px;
-    color:#899CB1;
-    cursor: pointer;
-    border:1px solid red;
-    &:hover {
-        color:#002CFB;
-    }
 `;
 
 export const StyledIconKeyboardArrowDown = styled(IconKeyboardArrowDown)`
-    padding:8px 6px;
-    width:16px;
-    height:16px;
-    margin-right: 40px;
-    border:1px solid red;
+  width: 24px;
+  height: 24px;
+  box-sizing: border-box;
+  margin-left: 4px;
+`;
+
+export const ContacName = styled.div`
+  display: flex;
+  align-items:center;
+  font-weight: 400;
+  line-height: 21px;
+  font-size: 15px;
+  color: #899cb1;
+  cursor: pointer;
+  margin-right: 41px;
+  &:hover {
+    color: #002cfb;
+  }
+  &:hover svg path{
+    fill: #002cfb;
+  }
 `;
 
 
+
+export const AnalyticsCalls = styled.div`
+  padding-top: 14px;
+  padding-bottom: 16px;
+
+  display: flex;
+  & > div {
+    margin-right: 56px;
+  }
+  & > div:last-child {
+    margin-right: 0px;
+  }
+`;
+
+export const Text = styled.div`
+  margin-bottom: 7px;
+  color: ${defaultTheme.colorBlackForText};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 148%; /* 20.72px */
+
+  & span {
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 148%;
+  }
+`;
+
+export const Line = styled.div`
+  width: 156px;
+  height: 6px;
+  border-radius: 12px 12px 12px 12px;
+  background-color: #d8e4fb;
+  border: none;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: 12px 12px 12px 12px;
+
+    width: ${(props) => props.completion * 100 + '%'};
+    height: 100%;
+  }
+`;
+
+export const NewCalls = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+
+  & ${Line}::after {
+    background-color: ${defaultTheme.colorGreen};
+  }
+  & ${Text} > span {
+    color: ${defaultTheme.colorGreen};
+  }
+`;
+
+export const QuantityCalls = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+
+  & ${Line}::after {
+    background-color: #ffd500;
+  }
+  & ${Text} > span {
+    color: #ffd500;
+  }
+`;
+
+export const СonversionToOrder = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+
+  & ${Line}::after {
+    background-color: ${defaultTheme.colorRed};
+  }
+  & ${Text} > span {
+    color: ${defaultTheme.colorRed};
+  }
+`;
+
+export const LeftBlock = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const RightBlock = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledSelect = styled.div`
+  margin-right: 41px;
+  & div:first-child > div:first-child {
+    color:#899CB1;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%; /* 21px */
+  }
+`;
