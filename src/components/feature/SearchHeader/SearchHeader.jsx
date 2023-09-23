18e73002a98f0d21415b6c43415afd6c983a297c
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:32:10 +0300

    update SearhHeader

---
+++ src/components/feature/SearchHeader/SearchHeader.jsx
@@ -0,0 +1,15 @@
+import { StyledContent } from "./styles";
+
+function Content() {
+  return (
+      <StyledContent>
+        {/* <Balannce />
+        <DatePicker />
+        <Search />
+        <Filter />
+        <Calls />         */}
+      </StyledContent>
+  );
+}
+
+export default Content;
