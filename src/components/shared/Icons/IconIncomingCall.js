2c9f7019b68ce7c81c60dd136a8604c577bc77b9
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:35:46 +0300

    aupdate IconIncomingCall

--- src/components/shared/Icons/IconIncomingCall.js
+++ src/components/shared/Icons/IconIncomingCall.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { ReactComponent as IncomingSvg } from "./../../../assets/icons/type-call/incoming-call.svg";
 
 export const IconIncomingCall = styled(IncomingSvg)`
-    width:12px;
-    height:12px;
-    padding:6px;
+    width:24px;
+    height:24px;
 `;
