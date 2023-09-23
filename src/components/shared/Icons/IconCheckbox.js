654734906c3939e7d7df6b94f6d3730a68dcaa92
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:34:13 +0300

    update IconcheckBox

--- src/components/shared/Icons/IconCheckbox.js
+++ src/components/shared/Icons/IconCheckbox.js
@@ -1,35 +1,34 @@
-import styled from "styled-components";
-import { ReactComponent as CheckboxSvg } from "./../../../assets/icons/checkbox/checkbox.svg";
-import { ReactComponent as CheckboxCheckedSvg } from "./../../../assets/icons/checkbox/checkbox-checked.svg";
-import { defaultTheme } from "../../../theme/default";
+import styled from 'styled-components';
+import {ReactComponent as CheckboxSvg} from './../../../assets/icons/checkbox/checkbox.svg';
+import {ReactComponent as CheckboxCheckedSvg} from './../../../assets/icons/checkbox/checkbox-checked.svg';
+import {defaultTheme} from '../../../theme/default';
 
 export const IconCheckbox = styled(CheckboxSvg)`
-    padding:4px;
-    width:16px;
-    height:16px;
-    
-    cursor: pointer;
-    path {
-        fill:${defaultTheme.colorCheckboxOffDefault};
-    }
-    &:hover path {
-        fill:${defaultTheme.colorCheckboxOffHover};
-    }
-    &:active path{ 
-        fill:${defaultTheme.colorCheckboxOffActive};
-    }
+  padding: 4px;
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+  position: relative;
+  path {
+    fill: ${defaultTheme.colorCheckboxOffDefault};
+  }
+  &:hover path {
+    fill: ${defaultTheme.colorCheckboxOffHover};
+  }
+  &:active path {
+    fill: ${defaultTheme.colorCheckboxOffActive};
+  }
 `;
 
 export const IconCheckboxChecked = styled(CheckboxCheckedSvg)`
-    padding:4px;
-    width:16px;
-    height:16px;
-    
-    cursor: pointer;
-    path {
-        fill:${defaultTheme.colorCheckboxOnDefault};
-    }
-    &:hover path{
-        fill:${defaultTheme.colorCheckboxOnHover};
-    }
+  padding: 4px;
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+  path {
+    fill: ${defaultTheme.colorCheckboxOnDefault};
+  }
+  &:active path {
+    fill: ${defaultTheme.colorCheckboxOffActive};
+  }
 `;
