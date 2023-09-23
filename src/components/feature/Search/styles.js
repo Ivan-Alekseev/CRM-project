f4d1b06454ec7b68a259ff5dcefba60ae5786d2a
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:31:13 +0300

    delete old Search, Icon Arrow, Logo mocks

--- src/components/feature/Search/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledContent = styled.div`
-  width: 1440px;
-  height:2120px;
-  margin:0 auto;
-  background-color:red;
-`;
--- src/components/shared/ui/Logo/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-
-export const StyledLogo = styled.a`
-  height: 80px;
-  padding-top:20px;
-  padding-left:12px;
-  text-decoration: none;
-`;
