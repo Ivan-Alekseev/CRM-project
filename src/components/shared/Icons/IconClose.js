ff330948ba1fc4d33e1b32e2472c7cede19537f9
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:34:28 +0300

    update IconClose

--- src/components/shared/Icons/IconClose.js
+++ src/components/shared/Icons/IconClose.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 import { ReactComponent as CloseSvg } from "./../../../assets/icons/music-player/button-close.svg";
+import { defaultTheme } from "../../../theme/default";
 
 export const IconClose = styled(CloseSvg)`
     width:24px;
     height:24px;
-    path {
-        fill:currentColor;
+    cursor: pointer;
+
+    path { 
+        fill:${defaultTheme.colorIconDefault};
     }
-    path:hover {
-        fill:currentColor;
+    &:hover path {
+        fill:${defaultTheme.colorIconHover};
+    }
+    &:active path {
+        fill:${defaultTheme.colorIconActive};
     }
 `;
