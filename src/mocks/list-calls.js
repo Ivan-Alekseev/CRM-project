b7b68181082aa1fa2340c49b6da38ef09790d01e
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:29:31 +0300

    add mock lise-calls

---
+++ src/mocks/list-calls.js
@@ -0,0 +1,219 @@
+export const listCallsMock = [
+  {
+    id:4888120, 
+    date: '2023-09-09 16:02:34',
+    ate_notime: '2023-09-09',
+    from_site: 0,
+    in_out: 1,
+    partnership_id: '578', //id партнера
+    partner_data: { //Маассив данных о партнере(id, name, phone).
+      id: '578',
+      name: 'ООО "ГРУЗЧИКОВ-СЕРВИС СПБ"',
+      phone: '74951205096',
+    },
+    person_avatar: '',
+    from_number: '796788878', //номер с которого был звонок
+  },
+];
+
+export const listCallsMocks = {
+  "total_rows": "468",
+    "results": [
+      {
+        "id": 11321321,
+        "partnership_id": "136",
+        "partner_data": {
+          "id": "336",
+          "name": "",
+          "phone": "7484xxxxxx"
+        },
+        "date": "2022-04-19 12:10:07",
+        "date_notime": "2022-04-19",
+        "time": 158,
+        "from_number": "79315315122",
+        "from_extension": "",
+        "to_number": "123",
+        "to_extension": "671",
+        "is_skilla": 0,
+        "status": "Дозвонился",
+        "record": "MToxMDA2NzYxNToxNDM0ODcwNDQzMzow",
+        "line_number": "sip:userxx@vpbx400105738.mangosip.ru",
+        "line_name": "название линии(источник)",
+        "in_out": 0,
+        "from_site": 1,
+        "source": "Rabota.ru",
+        "errors": [],
+        "disconnect_reason": "",
+        "results": [],
+        "stages": [],
+        "abuse": {
+          "date": "2022-05-17 14:35:05",
+          "person_name": "Никита",
+          "message": "Тестовая жалоба на звонок. Тест тест, можно не отвечать.",
+          "support_read_status": 1,
+          "support_answer_status": 1,
+          "answers": [
+            {
+              "message": "Уважаемый Никита. Проверили.",
+              "from_support": 1,
+              "support_read_status": 1,
+              "person_read_status": 1
+            }
+          ]
+        },
+        "contact_name": "Константиний Константинопольский",
+        "contact_company": "ООО «Фестивальный эффект»",
+        "person_id": 4042,
+        "person_name": "Татьяна",
+        "person_surname": "Михалкович",
+        "person_avatar": "https://esquimaltmfrc.com/wp-content/uploads/2015/09/flat-faces-icons-circle-man-9.png"
+      },
+      {
+        "id": 2123,
+        "partnership_id": "136",
+        "partner_data": {
+          "id": "336",
+          "name": "Алексей",
+          "phone": "7484xxxxxx"
+        },
+        "date": "2022-04-19 12:10:08",
+        "date_notime": "2022-04-19",
+        "time": 58,
+        "from_number": "79315xxxxxx",
+        "from_extension": "",
+        "to_number": "sip:hr_xxx@vpbx400105738.mangosip.ru",
+        "to_extension": "671",
+        "is_skilla": 0,
+        "status": "Дозвонился",
+        "record": "MToxMDA2NzYxNToxNDM0ODcwNDQzMzow",
+        "line_number": "sip:userxx@vpbx400105738.mangosip.ru",
+        "line_name": "название линии(источник)",
+        "in_out": 1,
+        "from_site": 0,
+        "source": "",
+        "errors": [],
+        "disconnect_reason": "",
+        "results": [],
+        "stages": [],
+        "abuse": {
+          "date": "2022-05-17 14:35:05",
+          "person_name": "Никита",
+          "message": "Тестовая жалоба на звонок. Тест тест, можно не отвечать.",
+          "support_read_status": 1,
+          "support_answer_status": 1,
+          "answers": [
+            {
+              "message": "Уважаемый Никита. Проверили.",
+              "from_support": 1,
+              "support_read_status": 1,
+              "person_read_status": 1
+            }
+          ]
+        },
+        "contact_name": "",
+        "contact_company": "",
+        "person_id": 4042,
+        "person_name": "Татьяна",
+        "person_surname": "Михалкович",
+        "person_avatar": "https://lk.skilla.ru/img/noavatar.jpg"
+      },
+      {
+        "id": 3123,
+        "partnership_id": "136",
+        "partner_data": {
+          "id": "336",
+          "name": "ИП Василек",
+          "phone": "7484xxxxxx"
+        },
+        "date": "2022-04-19 12:10:08",
+        "date_notime": "2022-04-19",
+        "time": 58,
+        "from_number": "79315315122",
+        "from_extension": "",
+        "to_number": "sip:hr_xxx@vpbx400105738.mangosip.ru",
+        "to_extension": "671",
+        "is_skilla": 0,
+        "status": "Дозвонился",
+        "record": "MToxMDA2NzYxNToxNDM0ODcwNDQzMzow",
+        "line_number": "sip:userxx@vpbx400105738.mangosip.ru",
+        "line_name": "название линии(источник)",
+        "in_out": 0,
+        "from_site": 1,
+        "source": "",
+        "errors": [],
+        "disconnect_reason": "",
+        "results": [],
+        "stages": [],
+        "abuse": {
+          "date": "2022-05-17 14:35:05",
+          "person_name": "Никита",
+          "message": "Тестовая жалоба на звонок. Тест тест, можно не отвечать.",
+          "support_read_status": 1,
+          "support_answer_status": 1,
+          "answers": [
+            {
+              "message": "Уважаемый Никита. Проверили.",
+              "from_support": 1,
+              "support_read_status": 1,
+              "person_read_status": 1
+            }
+          ]
+        },
+        "contact_name": "",
+        "contact_company": "",
+        "person_id": 4042,
+        "person_name": "Татьяна",
+        "person_surname": "Михалкович",
+        "person_avatar": "https://cm.author.today/content/2022/10/13/4ab77e96d69e413a9ea80374dea8787f.jpg"
+      },
+      {
+        "id": 4,
+        "partnership_id": "136",
+        "partner_data": {
+          "id": "336",
+          "name": "ИП Василек",
+          "phone": "7484xxxxxx"
+        },
+        "date": "2022-04-19 12:10:08",
+        "date_notime": "2022-04-19",
+        "time": 58,
+        "from_number": "79315xxxxxx",
+        "from_extension": "",
+        "to_number": "sip:hr_xxx@vpbx400105738.mangosip.ru",
+        "to_extension": "671",
+        "is_skilla": 0,
+        "status": "Дозвонился",
+        "record": "MToxMDA2NzYxNToxNDM0ODcwNDQzMzow",
+        "line_number": "sip:userxx@vpbx400105738.mangosip.ru",
+        "line_name": "название линии(источник)",
+        "in_out": 1,
+        "from_site": 0,
+        "source": "",
+        "errors": [],
+        "disconnect_reason": "",
+        "results": [],
+        "stages": [],
+        "abuse": {
+          "date": "2022-05-17 14:35:05",
+          "person_name": "Никита",
+          "message": "Тестовая жалоба на звонок. Тест тест, можно не отвечать.",
+          "support_read_status": 1,
+          "support_answer_status": 1,
+          "answers": [
+            {
+              "message": "Уважаемый Никита. Проверили.",
+              "from_support": 1,
+              "support_read_status": 1,
+              "person_read_status": 1
+            }
+          ]
+        },
+        "contact_name": "",
+        "contact_company": "",
+        "person_id": 4042,
+        "person_name": "Татьяна",
+        "person_surname": "Михалкович",
+        "person_avatar": "https://lk.skilla.ru/img/noavatar.jpg"
+      },
+    ]
+  }
