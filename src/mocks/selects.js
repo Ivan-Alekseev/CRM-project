7187dbb60fa5dac7535485a75c5ad2153de4315a
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-23 12:29:57 +0300

    add mock select

--- src/mocks/selects.js
+++ src/mocks/selects.js
@@ -30,3 +30,13 @@ export const allSources = {
     {value: 'otherCalls', label: 'Прочие звонки', selected: 'false'},
   ],
 };
+
+export const allOrganizations = {
+  label: 'Все организации',
+  options: [
+    {value: 'ИП Сидорова Александра Михайловна', label: 'ИП Сидорова Александра Михайловна', selected: 'true'},
+    {value: 'ООО Грузчиков Сервис Запад',label: 'ООО Грузчиков Сервис Запад',selected: 'false'},
+    {value: 'ИП Митрофанов М.М.', label: 'ИП Митрофанов М.М.', selected: 'false'},
+    {value: 'ИП Иванов М.М.', label: 'ИП Иванов М.М.', selected: 'false'},
+  ],
+};
