8d753ac4699e677a40168a1d7809a82045aa1132
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-24 22:58:24 +0300

    update page-wrapper

--- src/components/layout/page-wrapper/styles.js
+++ src/components/layout/page-wrapper/styles.js
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledPageWrapper = styled.div`
-  
   margin: 0;
   padding: 0;
   display: grid;
   grid-template-columns: 240px 1680px;
-  overflow: hidden;
 `;
 
 export const StyledContentWrapper = styled.div`
