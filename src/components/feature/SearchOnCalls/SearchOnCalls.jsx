4b26cfbfeb9d6415b94cafbd4db6948c54e6d608
Ivan Alekseev <ivanalekseev220195@gmail.com>
2023-09-24 23:51:53 +0300

    bug fix SearchOnCalls:add fn for click close

--- src/components/feature/SearchOnCalls/SearchOnCalls.jsx
+++ src/components/feature/SearchOnCalls/SearchOnCalls.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import { IconClose, IconSearch } from "../../shared/Icons";
 import { Input, Content } from "./styles";
 
+
+
 function SearchOnCalls() {
+
+  const [inputValue, setInputValue] = useState('');
+
+
+  const clearValue = () => {
+    setInputValue("");
+  }
+
   return (
       <Content>
         <IconSearch />
         <Input 
           type="tel"
           name="tel"
-          placeholder='Поиск по звонкам'
+          placeholder="Поиск по звонкам"
           pattern=""
-        />
-        <IconClose />
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+        }}/>
+        <IconClose onClick={()=> {
+          clearValue();
+        }}/>
       </Content>
   );
 }
